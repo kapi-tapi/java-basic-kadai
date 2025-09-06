06657977e2c08e79d86714d337560ae7138ce13d
kapi-tapi <askuvw@gmail.com>
2025-09-06 20:53:14 +0900

    kadai_015を作成

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,14 @@
+package katai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		Car_chapter15 mini = new Car_chapter15();
+		mini.cahgeGear(3);
+		mini.run();
+				
+		
+	}
+
+}
---
+++ kadai_015/Car_chapter15.java
@@ -0,0 +1,31 @@
+package katai_015;
+
+public class Car_chapter15 {
+
+	private int gear  = 1;  // 1速から5速のギアを表す
+	private int speed = 10; // ギアチェンジ後の速度を表す
+	
+	// メソッド ギアの値により速度を変える
+	public void cahgeGear( int afterGear ) {
+		System.out.println("ギア" + gear + "から" +
+							afterGear + "に切り替えました");
+		gear = afterGear;
+		switch(gear) {
+			case 1 -> speed = 10;
+			case 2 -> speed = 20;
+			case 3 -> speed = 30;
+			case 4 -> speed = 40;
+		default  -> speed = 50;
+		}
+	
+	}
+	
+	// メソッド　ギアチェンジ度の速度を表す
+	public void run() {
+		
+		System.out.println("速度は時速" + speed + "kmです");
+			
+		}
+
+	
+}
